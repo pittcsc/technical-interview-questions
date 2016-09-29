2090d412630090a1785f7cf273ee076f2b7c8815
Kyle Amoroso <kamoroso1994@gamil.com>
2016-09-29 19:52:21 -0400

    Added comments

--- remove-delimiters/solution.c
+++ remove-delimiters/solution.c
@@ -1,16 +1,19 @@
-void remove(char* s, char c) {
+void my_remove(char* s, char c) {
 	char* out;
 	
+	// nothing to do given a null pointer
 	if(!s) {
 		return;
 	}
 	
 	for(out = s; *s != '\0'; s++) {
+		// only write out valid characters
 		if(*s != c) {
 			*out = *s;
 			out++;
 		}
 	}
 	
+	// terminate string
 	*out = '\0';
 }
